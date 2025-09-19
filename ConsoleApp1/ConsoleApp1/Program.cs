cdc4d527427663acdaba7301f8887556d7039e8c
laptiidm <laptii.dm@gmail.com>
2025-09-19 13:05:41 +0300

    base commit

---
+++ ConsoleApp1/ConsoleApp1/Program.cs
@@ -0,0 +1,51 @@
+﻿internal class Program
+{
+	private static void Main(string[] args)
+	{
+		Console.Write("Введiть початкове значення Xmin: ");
+		string? sxMin = Console.ReadLine();
+		double xMin = double.Parse(sxMin ?? throw new ArgumentNullException(nameof(sxMin)));
+
+		Console.Write("Введiть кiнцеве значення Xmax: ");
+		string? sxMax = Console.ReadLine();
+		double xMax = double.Parse(sxMax ?? throw new ArgumentNullException(nameof(sxMax)));
+
+		Console.Write("Введiть прирiст dX: ");
+		string? sdx = Console.ReadLine();
+		double dx = double.Parse(sdx ?? throw new ArgumentNullException(nameof(sdx)));
+
+		double x = xMin;
+		double y;
+		double sum = 0;
+
+		while (x <= xMax)
+		{
+			double x1 = x;
+			double x2 = 3 * x;
+
+			y = (6 - Math.Cos(3 + x1)) / (34 - 9 * Math.Pow(x2, 3) + x2);
+
+			Console.WriteLine("x = {0}\t\ty = {1}", x, y);
+
+			sum += y; 
+			x += dx;
+		}
+
+		// 
+		if (Math.Abs(x - xMax - dx) > 0.0001)
+		{
+			double x1 = xMax;
+			double x2 = 3 * xMax;
+
+			y = (6 - Math.Cos(3 + x1)) / (34 - 9 * Math.Pow(x2, 3) + x2);
+
+			Console.WriteLine("x = {0}\t\ty = {1}", xMax, y);
+
+			sum += y;
+		}
+
+		Console.WriteLine("\nСума всiх значень f(x) = {0}", sum);
+
+		Console.ReadKey();
+	}
+}
---
+++ ConsoleApp2/ConsoleApp2/Program.cs
@@ -0,0 +1,36 @@
+﻿internal class Program
+{
+	private static void Main(string[] args)
+	{
+		Console.WriteLine("Введіть X1min, X1max і dx1 через пробіл:");
+		string[] parts1 = Console.ReadLine()!.Split(' ');
+
+		double x1min = Convert.ToDouble(parts1[0]);
+		double x1max = Convert.ToDouble(parts1[1]);
+		double dx1 = Convert.ToDouble(parts1[2]);
+
+		Console.WriteLine("Введіть X2min, X2max і dx2 через пробіл:");
+		string[] parts2 = Console.ReadLine()!.Split(' ');
+
+		double x2min = Convert.ToDouble(parts2[0]);
+		double x2max = Convert.ToDouble(parts2[1]);
+		double dx2 = Convert.ToDouble(parts2[2]);
+
+		double sumOfAllIntermediateResults = 0;
+
+		for (double x1 = x1min; x1 <= x1max; x1 += dx1)
+		{
+			for (double x2 = x2min; x2 <= x2max; x2 += dx2)
+			{
+
+				double result = (3 * x2 - Math.Pow(x1, 2)) / Math.Pow(Math.Cos((x1 + 2 * x2 + 9) / 0.37), 3);
+				Console.WriteLine("{0,10:F3} | {1,10:F3} | {2,15:F6}", x1, x2, result);
+
+				if (result > 0) { sumOfAllIntermediateResults += result; }
+			}
+		}
+
+		Console.WriteLine(new string('-', 40));
+		Console.WriteLine("Сума додатних результатів: {0:F6}", sumOfAllIntermediateResults);
+	}
+}
