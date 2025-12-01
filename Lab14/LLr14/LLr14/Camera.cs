4f816d56dd75c79a0bb867df8dba8066e8363668
laptiidm <laptii.dm@gmail.com>
2025-12-02 01:31:27 +0200

    lab 14 oop

---
+++ Lab14/LLr14/LLr14/Camera.cs
@@ -0,0 +1,44 @@
+﻿using System;
+
+[Serializable]
+public class Camera : IComparable<Camera>
+{
+	public string Model { get; set; }
+	public string Manufacturer { get; set; }
+	public double Megapixels { get; set; }
+	public int BatteryCapacity { get; set; }
+	public double Weight { get; set; }
+	public decimal Price { get; set; }
+	public int ReleaseYear { get; set; }
+	public bool HasWiFi { get; set; }
+	public bool HasInterchangeableLens { get; set; }
+
+	public Camera() { }
+
+	public Camera(string model, string manufacturer, double mp, int battery,
+				  double weight, decimal price, int year, bool wifi, bool lens)
+	{
+		Model = model;
+		Manufacturer = manufacturer;
+		Megapixels = mp;
+		BatteryCapacity = battery;
+		Weight = weight;
+		Price = price;
+		ReleaseYear = year;
+		HasWiFi = wifi;
+		HasInterchangeableLens = lens;
+	}
+
+	public int CompareTo(Camera other)
+	{
+		if (other == null) return 1;
+		return Model.CompareTo(other.Model);
+	}
+
+	public override string ToString()
+	{
+		return $"{Model,-20}  {Manufacturer,-10}  {Megapixels,5} Mp  " +
+			   $"{BatteryCapacity,5} mAh  {Weight,5} g  {Price,8}$  " +
+			   $"{ReleaseYear}  WiFi:{HasWiFi}  Lens:{HasInterchangeableLens}";
+	}
+}
---
+++ Lab14/LLr14/LLr14/Program.cs
@@ -0,0 +1,95 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+
+class Program
+{
+	static void Main()
+	{
+
+		Console.OutputEncoding = System.Text.Encoding.UTF8;
+
+		string filePath = @"C:\Onedrive\Desktop\somecamBIN.cams";
+
+		if (!File.Exists(filePath))
+		{
+			Console.WriteLine("Файл не знайдено: " + filePath);
+			return;
+		}
+
+		List<Camera> cameras = LoadFromBinary(filePath);
+
+		Console.WriteLine("=== Дані, прочитані з BIN ===");
+		Print(cameras);
+
+		cameras.Sort();
+		Console.WriteLine("\n=== Після сортування ===");
+		Print(cameras);
+
+		cameras.Add(new Camera("Alpha A7IV", "Sony", 33, 2280, 658, 2499, 2021, true, true));
+		cameras.Sort();
+
+		Console.WriteLine("\n=== Після додавання нової камери ===");
+		Print(cameras);
+
+		if (cameras.Count > 0)
+			cameras.RemoveAt(cameras.Count - 1);
+
+		Console.WriteLine("\n=== Після видалення останньої камери ===");
+		Print(cameras);
+
+		SaveToBinary(filePath, cameras);
+
+		Console.WriteLine("\nГотово.");
+	}
+
+	static void Print(List<Camera> cams)
+	{
+		foreach (var cam in cams)
+			Console.WriteLine(cam);
+	}
+
+	static List<Camera> LoadFromBinary(string path)
+	{
+		List<Camera> list = new List<Camera>();
+
+		using (var br = new BinaryReader(File.OpenRead(path)))
+		{
+			while (br.BaseStream.Position < br.BaseStream.Length)
+			{
+				string model = br.ReadString();
+				string manufacturer = br.ReadString();
+				double mp = br.ReadDouble();
+				int battery = br.ReadInt32();
+				double weight = br.ReadDouble();
+				decimal price = br.ReadDecimal();
+				int year = br.ReadInt32();
+				bool wifi = br.ReadBoolean();
+				bool lens = br.ReadBoolean();
+
+				list.Add(new Camera(model, manufacturer, mp, battery, weight, price, year, wifi, lens));
+			}
+		}
+
+		return list;
+	}
+
+	static void SaveToBinary(string path, List<Camera> cams)
+	{
+		using (var bw = new BinaryWriter(File.Create(path)))
+		{
+			foreach (var cam in cams)
+			{
+				bw.Write(cam.Model);
+				bw.Write(cam.Manufacturer);
+				bw.Write(cam.Megapixels);
+				bw.Write(cam.BatteryCapacity);
+				bw.Write(cam.Weight);
+				bw.Write(cam.Price);
+				bw.Write(cam.ReleaseYear);
+				bw.Write(cam.HasWiFi);
+				bw.Write(cam.HasInterchangeableLens);
+			}
+		}
+	}
+}
